62c9b119fa817acbc6c816d6874084443e1cbb15
Paola Díaz <45045278+paodiazs@users.noreply.github.com>
2024-12-13 23:07:12 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,32 @@
+function toggleMenu(){
+    /*we are targetting these two elements*/
+    const menu = document.querySelector(".menu-links");
+    const icon = document.querySelector(".hamburguer-icon");
+    /*when we click it  */
+    menu.classList.toggle("open");
+    icon.classList.toggle("open");
+}
+function redirectToAfrodia() {
+    
+    window.open('https://paodiazs.github.io/AfrodIA/', '_blank');
+}
+function redirectToGitHubTuriOax(){
+    window.open('https://github.com/paodiazs/appturioax.git', '_blank');
+}
+function redirectToPandasPython(){
+    window.open('https://github.com/paodiazs/PANDAS_Analysis.git', '_blank');
+}
+function redirectToGitHub(){
+    window.open('https://github.com/paodiazs', '_blank');
+}
+function redirectTolinkedin(){
+    window.open('https://www.linkedin.com/in/paola-diaz-ser/', '_blank');
+}
+
+function redirectToMedium(){
+    window.open('https://medium.com/@paodiazserrano', '_blank');
+}
+
+function redirectToMarianaCampestre(){
+    window.open('https://restaurantecampestremariana.com/', '_blank');
+}
